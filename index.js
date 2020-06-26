af4beb41fa0b5dad827f3a901f08c4fde4f8e6fd
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-26 20:54:50 +0530

    Fetching Data From API

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 window.addEventListener('DOMContentLoaded', function () {
     console.log("Running")
+    const API_URL = "https://starwars.egghead.training/";
+
+    const output = document.getElementById("output")
+
+    output.innerText = 'Loading...'
+
+    const responsePromise = fetch(API_URL + 'films')
+
+    responsePromise.then(response => {
+        return response.json()
+    }).then(films => {
+        const filmTitles = films
+            .sort((a, b) => a.episode_id > b.episode_id)
+            .map(film => `${film.episode_id}. ${film.title}`)
+            .join("\n");
+
+        output.innerText = filmTitles
+
+    })
+
 })
